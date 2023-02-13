6ca62d1a57682a679783a3b94de7e065df7f3bd9
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-02-13 17:58:13 +0300

    feat: set login section on full screen by css

--- src/pages/loginPage/login.ts
+++ src/pages/loginPage/login.ts
@@ -1,3 +1,5 @@
+const loginImage = require('../../assets/images/login-image.jpg');
+
 export class LoginPage {
   parent: HTMLElement;
 
@@ -8,14 +10,14 @@ export class LoginPage {
 
   createElement() {
     const loginSection = document.createElement('section');
-    loginSection.className = 'flex flex-col md:flex-row items-center';
+    loginSection.className = 'flex flex-col md:flex-row h-screen items-center fixed top-0 left-0 right-0 bottom-0 z-50';
     loginSection.innerHTML = `
-      <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3">
-        <img src="https://source.unsplash.com/random" alt="" class="w-full h-full object-cover">
+      <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+        <img src="${loginImage}" alt="login page background" class="w-full h-full object-cover">
       </div>
 
-      <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 px-6 lg:px-16 xl:px-12
-            flex items-center justify-center">
+      <div class="bg-white w-full md:mx-auto md:mx-0 md:w-full xl:w-1/3 lg:w-1/2 h-screen px-6 lg:px-16 xl:px-12
+      flex items-center justify-center">
 
         <div class="w-full h-100">
           <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
