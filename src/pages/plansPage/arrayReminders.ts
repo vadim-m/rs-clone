7a17bf2ebe46df98b9d9d203912d16af7bda4cd2
D1van007 <d1van28.01.1987@gmail.com>
2023-03-02 18:20:33 +0300

    fix: calculate cost fuel

--- src/pages/plansPage/arrayReminders.ts
+++ src/pages/plansPage/arrayReminders.ts
@@ -8,231 +8,190 @@ const carRepair = require('../../assets/icons/car-repair-grey.svg');
 const carRepairColor = require('../../assets/icons/car-repair-color.svg');
 
 // График ТО
-const changingOil: IParamsOneReminder = {
-  class: 'changingOil',
-  textName: eventLang().changingOil,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'changingOil',
-};
+export function collectionPlansArr(): IParamsOneReminder[] {
+  const changingOil: IParamsOneReminder = {
+    class: 'changingOil',
+    textName: eventLang().changingOil,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'changingOil',
+  };
 
-const airFilter: IParamsOneReminder = {
-  class: 'airFilter',
-  textName: eventLang().airFilter,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'airFilter',
-};
+  const airFilter: IParamsOneReminder = {
+    class: 'airFilter',
+    textName: eventLang().airFilter,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'airFilter',
+  };
 
-const fuelFilter: IParamsOneReminder = {
-  class: 'fuelFilter',
-  textName: eventLang().fuelFilter,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'fuelFilter',
-};
+  const fuelFilter: IParamsOneReminder = {
+    class: 'fuelFilter',
+    textName: eventLang().fuelFilter,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'fuelFilter',
+  };
 
-const interiorFilter: IParamsOneReminder = {
-  class: 'interiorFilter',
-  textName: eventLang().interiorFilter,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'interiorFilter',
-};
-const brakePadsFront: IParamsOneReminder = {
-  class: 'brakePadsFront',
-  textName: eventLang().brakePadsFront,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'brakePadsFront',
-};
-const brakePadsRear: IParamsOneReminder = {
-  class: 'brakePadsRear',
-  textName: eventLang().brakePadsRear,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'brakePadsRear',
-};
-const brakeFluid: IParamsOneReminder = {
-  class: 'brakeFluid',
-  textName: eventLang().brakeFluid,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'brakeFluid',
-};
-const timingBelts: IParamsOneReminder = {
-  class: 'timingBelts',
-  textName: eventLang().timingBelts,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'timingBelts',
-};
-const sparkPlugs: IParamsOneReminder = {
-  class: 'sparkPlugs',
-  textName: eventLang().sparkPlugs,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'sparkPlugs',
-};
-const antifreeze: IParamsOneReminder = {
-  class: 'antifreeze',
-  textName: eventLang().antifreeze,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'antifreeze',
-};
-const transmissionOil: IParamsOneReminder = {
-  class: 'transmissionOil',
-  textName: eventLang().transmissionOil,
-  textType: eventLang().maintenance,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'transmissionOil',
-};
-const diagnosticsSuspension: IParamsOneReminder = {
-  class: 'diagnosticsSuspension',
-  textName: eventLang().diagnosticsSuspension,
-  textType: eventLang().diagnostics,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'diagnosticsSuspension',
-};
-const diagnosticsElectrical: IParamsOneReminder = {
-  class: 'antifreeze',
-  textName: eventLang().diagnosticsElectrical,
-  textType: eventLang().diagnostics,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'diagnosticsElectrical',
-};
-const winterTires: IParamsOneReminder = {
-  class: 'winterTires',
-  textName: eventLang().winterTires,
-  textType: eventLang().replacement,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'winterTires',
-};
-const summerTires: IParamsOneReminder = {
-  class: 'summerTires',
-  textName: eventLang().summerTires,
-  textType: eventLang().replacement,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'summerTires',
-};
-const carBattery: IParamsOneReminder = {
-  class: 'carBattery',
-  textName: eventLang().carBattery,
-  textType: eventLang().replacement,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'carBattery',
-};
-const techInspect: IParamsOneReminder = {
-  class: 'techInspect',
-  textName: eventLang().techInspect,
-  textType: eventLang().other,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'techInspect',
-};
-const tax: IParamsOneReminder = {
-  class: 'tax',
-  textName: eventLang().tax,
-  textType: eventLang().other,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'tax',
-};
-const inshurance: IParamsOneReminder = {
-  class: 'inshurance',
-  textName: eventLang().inshurance,
-  textType: eventLang().other,
-  icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
-  reminderDefault: true,
-  id: 'inshurance',
-};
+  const interiorFilter: IParamsOneReminder = {
+    class: 'interiorFilter',
+    textName: eventLang().interiorFilter,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'interiorFilter',
+  };
+  const brakePadsFront: IParamsOneReminder = {
+    class: 'brakePadsFront',
+    textName: eventLang().brakePadsFront,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'brakePadsFront',
+  };
+  const brakePadsRear: IParamsOneReminder = {
+    class: 'brakePadsRear',
+    textName: eventLang().brakePadsRear,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'brakePadsRear',
+  };
+  const brakeFluid: IParamsOneReminder = {
+    class: 'brakeFluid',
+    textName: eventLang().brakeFluid,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'brakeFluid',
+  };
+  const timingBelts: IParamsOneReminder = {
+    class: 'timingBelts',
+    textName: eventLang().timingBelts,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'timingBelts',
+  };
+  const sparkPlugs: IParamsOneReminder = {
+    class: 'sparkPlugs',
+    textName: eventLang().sparkPlugs,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'sparkPlugs',
+  };
+  const antifreeze: IParamsOneReminder = {
+    class: 'antifreeze',
+    textName: eventLang().antifreeze,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'antifreeze',
+  };
+  const transmissionOil: IParamsOneReminder = {
+    class: 'transmissionOil',
+    textName: eventLang().transmissionOil,
+    textType: eventLang().maintenance,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'transmissionOil',
+  };
+  const diagnosticsSuspension: IParamsOneReminder = {
+    class: 'diagnosticsSuspension',
+    textName: eventLang().diagnosticsSuspension,
+    textType: eventLang().diagnostics,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'diagnosticsSuspension',
+  };
+  const diagnosticsElectrical: IParamsOneReminder = {
+    class: 'antifreeze',
+    textName: eventLang().diagnosticsElectrical,
+    textType: eventLang().diagnostics,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'diagnosticsElectrical',
+  };
+  const winterTires: IParamsOneReminder = {
+    class: 'winterTires',
+    textName: eventLang().winterTires,
+    textType: eventLang().replacement,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'winterTires',
+  };
+  const summerTires: IParamsOneReminder = {
+    class: 'summerTires',
+    textName: eventLang().summerTires,
+    textType: eventLang().replacement,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'summerTires',
+  };
+  const carBattery: IParamsOneReminder = {
+    class: 'carBattery',
+    textName: eventLang().carBattery,
+    textType: eventLang().replacement,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'carBattery',
+  };
+  const techInspect: IParamsOneReminder = {
+    class: 'techInspect',
+    textName: eventLang().techInspect,
+    textType: eventLang().other,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'techInspect',
+  };
+  const tax: IParamsOneReminder = {
+    class: 'tax',
+    textName: eventLang().tax,
+    textType: eventLang().other,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'tax',
+  };
+  const inshurance: IParamsOneReminder = {
+    class: 'inshurance',
+    textName: eventLang().inshurance,
+    textType: eventLang().other,
+    icon: `<img src="${carRepair}" class="h-8 w-8" alt="car-repair icon">`,
+    reminderDefault: true,
+    id: 'inshurance',
+  };
 
-export const maintenanceArr = [
-  changingOil,
-  airFilter,
-  fuelFilter,
-  interiorFilter,
-  brakePadsFront,
-  brakePadsRear,
-  brakeFluid,
-  timingBelts,
-  sparkPlugs,
-  antifreeze,
-  transmissionOil,
-  diagnosticsSuspension,
-  diagnosticsElectrical,
-  winterTires,
-  summerTires,
-  carBattery,
-  techInspect,
-  tax,
-  inshurance,
-];
-
+  return [
+    changingOil,
+    airFilter,
+    fuelFilter,
+    interiorFilter,
+    brakePadsFront,
+    brakePadsRear,
+    brakeFluid,
+    timingBelts,
+    sparkPlugs,
+    antifreeze,
+    transmissionOil,
+    diagnosticsSuspension,
+    diagnosticsElectrical,
+    winterTires,
+    summerTires,
+    carBattery,
+    techInspect,
+    tax,
+    inshurance,
+  ];
+}
 export function createArrPlans(showPlansValue: string): IParamsOneReminder[] {
   const myCarData: ICarData = localStorage.getItem('car') ? JSON.parse(localStorage.getItem('car') as string) : carData;
   const myReminderArr: IReminders[] = myCarData.event.reminders;
   const myRemindersPlans: IParamsOneReminder[] = [];
   const myRemindersDefault: IParamsOneReminder[] = [];
-  const nameArr = [
-    eventLang().changingOil,
-    eventLang().airFilter,
-    eventLang().fuelFilter,
-    eventLang().interiorFilter,
-    eventLang().brakePadsFront,
-    eventLang().brakePadsRear,
-    eventLang().brakeFluid,
-    eventLang().timingBelts,
-    eventLang().sparkPlugs,
-    eventLang().antifreeze,
-    eventLang().transmissionOil,
-    eventLang().diagnosticsSuspension,
-    eventLang().diagnosticsElectrical,
-    eventLang().winterTires,
-    eventLang().summerTires,
-    eventLang().carBattery,
-    eventLang().techInspect,
-    eventLang().tax,
-    eventLang().inshurance,
-  ];
-  const typeArr = [
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().maintenance,
-    eventLang().diagnostics,
-    eventLang().diagnostics,
-    eventLang().replacement,
-    eventLang().replacement,
-    eventLang().replacement,
-    eventLang().other,
-    eventLang().other,
-    eventLang().other,
-  ];
 
   myReminderArr.forEach((e) => {
     return myRemindersPlans.push({
@@ -251,11 +210,11 @@ export function createArrPlans(showPlansValue: string): IParamsOneReminder[] {
       id: e.id,
     });
   });
-  maintenanceArr.forEach((e, i) => {
+  collectionPlansArr().forEach((e) => {
     return myRemindersDefault.push({
       class: e.class,
-      textName: nameArr[i],
-      textType: typeArr[i],
+      textName: e.textName,
+      textType: e.textType,
       icon: e.icon,
       reminderDefault: true,
       id: e.id,
